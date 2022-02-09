e5edd00ca59ac9eb60e7d0f0f13f75cf6ac173f5
Richard Whitehill <whiteric@jlab.org>
2022-02-08 18:31:48 -0600

    update to Program 3

--- Program3/analyze.for
+++ Program3/analyze.for
@@ -1,21 +1,23 @@
        program analyze
 
        implicit none
-
        integer,allocatable :: date(:)
        integer,allocatable :: odometer(:)
        real,allocatable :: fuel(:)
        integer :: i,io,x,y
        real :: z
 
-       open(1,file='car.dat')
-       
+       double precision :: avg,average,stdev
+       integer :: get_year
+
+       open(10,file='car.dat')
+
        i = 0
        allocate(date(0))
        allocate(odometer(0))
        allocate(fuel(0))
        do
-           read(1,*,iostat=io)x,y,z
+           read(10,*,iostat=io)x,y,z
            if (io == 0) then
                i = i + 1
                date = [date,x]
@@ -26,15 +28,38 @@
                exit
            end if
        end do
-       close(1)
+       close(10)
+       average = avg(fuel,size(fuel))
+1      format(f6.3,A,f6.3)
+       write(*,'(A)')'average fuel inserted: '
+       write(*,'(f6.3,A,f5.3)')average,' +- ',stdev(fuel,size(fuel))
        do i = 1,size(date)
-           write(*,*)date(i),odometer(i),fuel(i)
+           write(*,*)get_year(date(i))!,odometer(i),fuel(i)
        end do
 
-       avg(date)
-
-       integer avg(arr)
-           write(*,*)size(arr)
-       return
-
        end program analyze
+
+!====================
+
+       function avg(arr,arr_size)
+           implicit none
+           integer :: arr_size
+           real :: arr(arr_size)
+           double precision :: avg
+           avg = sum(arr)/arr_size
+       end function
+
+       function stdev(arr,arr_size)
+           implicit none
+           integer :: arr_size
+           real :: arr(arr_size)
+           double precision :: avg,stdev
+           stdev = sqrt(sum((arr-avg(arr,arr_size))**2)/(arr_size-1))
+       end function
+
+       function get_year(date)
+           implicit none
+           integer :: date
+           integer :: get_year
+           get_year = date/1e4
+       end function
